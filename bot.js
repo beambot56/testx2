2df3d31c20776522102307c1de959aee4ed25685
beambot56 <46430236+beambot56@users.noreply.github.com>
2019-01-06 21:08:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,16 @@ client.on('message', message => {
 
        }
 
+    if (message.content === '?say') {
+ 
+     
+    const sayMessage = args.join(" ");
+ 
+    message.delete().catch(O_o=>{}); 
+    
+    message.channel.send(sayMessage);
+  }
+
 });
 
  
