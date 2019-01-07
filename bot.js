6876cfd1806d206eb38ab1ab54ef13f958a11a47
beambot56 <46430236+beambot56@users.noreply.github.com>
2019-01-06 21:26:07 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,6 +25,11 @@ client.on('message', message => {
         message.reply('Here is a freshly baked cookie from me! <3')
 
        }
+  
+    if (message.content === '?anthem') {
+
+        message.reply('United forever in friendship and labour, our mighty republics will ever endure. The Great Soviet Union will live through the ages. The dream of a people their fortress secure. Long live our Soviet motherland, built by the peoples mighty hand. Long live our people, united and free. Strong in our friendship tried by fire. Long may our crimson flag inspire, shining in glory for all men to see. Through days dark and stormy where Great Lenin lead us our eyes saw the bright sun of freedom above and Stalin our leader with faith in the people, inspired us to build up the land that we love. Long live our Soviet motherland, built by the peoples mighty hand. Long live our people, united and free. Strong in our friendship tried by fire. Long may our crimson flag inspire, shining in glory for all men to see. We fought for the future, destroyed the invaders, and brought to our homeland the laurels of fame. Our glory will live in the memory of nations and all generations will honour her name. Long live our Soviet motherland, built by the peoples mighty hand. Long live our people, united and free. Strong in our friendship tried by fire. Long may our crimson flag inspire, shining in glory for all men to see.')
+       }
 });
 
  
